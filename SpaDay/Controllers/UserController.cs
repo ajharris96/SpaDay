5ca7432a82d559743276521e2ed4490119a81282
andrew <andrewjharris96@gmail.com>
2021-11-01 21:03:48 -0400

    working?

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            if(ViewBag.passverified == null)
+            {
+                ViewBag.passverified = true;
+            }
+            
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+
+            ViewBag.username = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.date = newUser.Created;
+            if (newUser.VerifyPassword(verify))
+            {
+                ViewBag.passverified = true;
+                
+                return View("Index");
+            }
+             
+          
+            ViewBag.passverified = false;
+            return View("Add");
+            
+            
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime? Created { get; }
+        public DateTime dateTime = DateTime.Now;
+
+        public User()
+        {
+        }
+
+        public User(string username, string email, string password, DateTime dateTime)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            Created = dateTime;
+        }
+
+        public bool VerifyPassword(string verify)
+        {
+            return Password == verify;
+        }
+    }
+}
