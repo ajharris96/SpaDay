bbe7a78b3772ca3bbb22e4d9f4bbc2d1a9ffc2e9
andrew <andrewjharris96@gmail.com>
2021-11-04 21:09:49 -0400

    all done

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
+using SpaDay.ViewModels;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,26 +21,28 @@ namespace SpaDay.Controllers
 
         public IActionResult Add()
         {
-            return View();
+            
+            AddUserViewModel userViewModel = new AddUserViewModel();
+            return View(userViewModel);
         }
 
         [HttpPost]
         [Route("/user")]
-        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        public IActionResult SubmitAddUserForm(AddUserViewModel userViewModel)
         {
-            if (newUser.Password == verify)
+            if (ModelState.IsValid)
             {
-                ViewBag.user = newUser;
-                return View("Index");
+                User newUser = new User(userViewModel.Username, userViewModel.Email, userViewModel.Password);
+                
+                
+                return View("Index", newUser);
             }
-            else
-            {
-                ViewBag.error = "Passwords do not match! Try again!";
-                ViewBag.userName = newUser.Username;
-                ViewBag.eMail = newUser.Email;
-                return View("Add");
-            }
+            
+            else return View("Add", userViewModel);
+        }
+
+       
         }
 
     }
-}
+
---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SpaDay;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 15 characters.")]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage ="Must be a proper email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage ="Password must be between 6 and 20 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Verify Password")]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        //[StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        public string VerifyPassword { get; set; }
+
+        public AddUserViewModel()
+        {
+        }
+    }
+}
